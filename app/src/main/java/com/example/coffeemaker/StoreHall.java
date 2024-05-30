c3eb02aedc1d49e3b8b104d37d55f826ab57de65
ClaudeMonetOscar <hagis0405@gachon.ac.kr>
2024-05-30 16:20:27 +0900

    add new menu

--- app/src/main/java/com/example/coffeemaker/StoreHall.java
+++ app/src/main/java/com/example/coffeemaker/StoreHall.java
@@ -36,9 +36,9 @@ public class StoreHall extends AppCompatActivity {
     int beverage_completion;//제조음료 완성도
     Random random = new Random();
 
-    Boolean waterOn, milkOn, coffeeOn, iceOn, vanillaOn, lemonOn;//제조음료의 재료 포함 여부
+    Boolean waterOn, milkOn, coffeeOn, iceOn, vanillaOn, lemonOn, matchaOn, strawberryOn;//제조음료의 재료 포함 여부
 
-    Intent firstIntent, secondIntent, thirdIntent, fourthIntent;//firstIntent는 kitchen에서 데이터 받아오는 인텐트, secondIntent는 kitchen으로 데이터 보내고 화면 전환하는 인텐트
+    Intent firstIntent, secondIntent, thirdIntent;//firstIntent는 kitchen에서 데이터 받아오는 인텐트, secondIntent는 kitchen으로 데이터 보내고 화면 전환하는 인텐트
 
 
     @Override
@@ -58,14 +58,14 @@ public class StoreHall extends AppCompatActivity {
         getIntent = firstIntent.getIntExtra("intent_index", -1);
         past_order = firstIntent.getIntExtra("order_index", -1);
         past_client = firstIntent.getIntExtra("client_idx", -1);
-        past_weather = firstIntent.getIntExtra("weather_index", -5);
+        //past_weather = firstIntent.getIntExtra("weather_index", -5);
         weather = firstIntent.getIntExtra("weather_index", -5);
-        Log.d("past weather", past_weather+"");
+        time = firstIntent.getIntExtra("time_index", -1);
         Log.d("weather", weather+"");
 
         if(getIntent==1) {
 
-            showWeather(past_weather);
+            showBackground(weather);
             //주문했던 손님의 이미지 다시 표시
             showClient(past_order, past_client, 0, getIntent, clientImg, clientOrder, moveKitchen);
             //이용자가 만든 음료 완성도 확인
@@ -80,6 +80,7 @@ public class StoreHall extends AppCompatActivity {
                 public void onClick(View v) {
                     thirdIntent = new Intent(getApplicationContext(), StoreHall.class);
                     thirdIntent.putExtra("weather_index", weather);
+                    thirdIntent.putExtra("time_index", time);
                     startActivity(thirdIntent);
                     finish();
                 }
@@ -88,10 +89,10 @@ public class StoreHall extends AppCompatActivity {
 
         if(getIntent!=1) {
             time = random.nextInt(5000);
-            order = random.nextInt(10);
+            order = random.nextInt(12);
             clientNum = random.nextInt(7);
 
-            showWeather(weather);//날씨별 배경 표시                        ////////////////////////
+            showBackground(weather);//날씨별 배경 표시                        ////////////////////////
             showOrder(order);//손님의 주문 표시
             showClient(order, clientNum, time, getIntent, clientImg, clientOrder, moveKitchen);//손님 이미지 표시
         }
@@ -103,25 +104,23 @@ public class StoreHall extends AppCompatActivity {
                 secondIntent.putExtra("order_idx", order);
                 secondIntent.putExtra("client_idx", clientNum);
                 secondIntent.putExtra("weather_index", weather);
+                secondIntent.putExtra("time_index", time);
                 startActivity(secondIntent);
             }
         });
     }
     private class Beverage {
-        boolean water;
-        boolean milk;
-        boolean ice;
-        boolean coffee;
-        boolean vanilla;
-        boolean lemon;
+        boolean water, milk, ice, coffee, vanilla, lemon, matcha, strawberry;
 
-        private Beverage(boolean water, boolean milk, boolean coffee, boolean ice, boolean vanilla, boolean lemon) {
+        private Beverage(boolean water, boolean milk, boolean coffee, boolean ice, boolean vanilla, boolean lemon, boolean matcha, boolean strawberry) {
             this.water=water;
             this.milk=milk;
             this.coffee=coffee;
             this.ice=ice;
             this.vanilla=vanilla;
             this.lemon=lemon;
+            this.matcha=matcha;
+            this.strawberry=strawberry;
         }
     }
     private int checkComplication(Intent firstIntent) {
@@ -131,17 +130,25 @@ public class StoreHall extends AppCompatActivity {
         iceOn = firstIntent.getBooleanExtra("is_iceOn", false);
         vanillaOn = firstIntent.getBooleanExtra("is_vanillaOn", false);
         lemonOn = firstIntent.getBooleanExtra("is_lemonOn", false);
+        matchaOn = firstIntent.getBooleanExtra("is_matchaOn", false);
+        strawberryOn = firstIntent.getBooleanExtra("is_strawberryOn", false);
 
+        if (!waterOn&!milkOn&!coffeeOn&!iceOn&!vanillaOn&!lemonOn&!matchaOn&!strawberryOn) {
+            beverage_completion=-1;//아무 재료도 없는 경우
+            return beverage_completion;
+        }
         userBeverage = checkRecipe(past_order);
 
         // 유저가 만든 음료와 정답 음료 정확도 비교
-        beverage_completion = 6;
+        beverage_completion = 8;
         if (userBeverage.ice != iceOn) beverage_completion--;
         if (userBeverage.water != waterOn) beverage_completion--;
         if (userBeverage.coffee != coffeeOn) beverage_completion--;
         if (userBeverage.milk != milkOn) beverage_completion--;
         if (userBeverage.vanilla != vanillaOn) beverage_completion--;
         if (userBeverage.lemon != lemonOn) beverage_completion--;
+        if (userBeverage.matcha != matchaOn) beverage_completion--;
+        if (userBeverage.strawberry != strawberryOn) beverage_completion--;
 
         return beverage_completion;
     }
@@ -153,6 +160,8 @@ public class StoreHall extends AppCompatActivity {
         boolean answer_milk = true;
         boolean answer_vanilla = true;
         boolean answer_lemon = true;
+        boolean answer_matcha = true;
+        boolean answer_strawberry = true;
 
         // 주문 확인하는 코드.
         if(order == 0 ){//아이스 아메리카노
@@ -162,6 +171,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = false;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if(order == 1 ){//핫 아메리카노
             answer_ice = false;
@@ -170,6 +181,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = false;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order == 2) {//아이스 라떼
             answer_ice = true;
@@ -178,6 +191,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = true;
             answer_vanilla = false;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order == 3) {//핫 라떼
             answer_ice = false;
@@ -186,6 +201,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = true;
             answer_vanilla = false;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==4) {//아이스 바닐라라떼
             answer_ice = true;
@@ -194,6 +211,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = true;
             answer_vanilla = true;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==5) {//핫 바닐라라떼
             answer_ice = false;
@@ -202,6 +221,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = true;
             answer_vanilla = true;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==6) {//아이스 바닐라아메리카노
             answer_ice = true;
@@ -210,6 +231,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = true;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==7) {//핫 바닐라아메리카노
             answer_ice = false;
@@ -218,6 +241,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = true;
             answer_lemon = false;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==8) {//아이스 레몬차
             answer_ice = true;
@@ -226,6 +251,8 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = false;
             answer_lemon = true;
+            answer_matcha = false;
+            answer_strawberry = false;
         }
         else if (order==9) {//핫 레몬차
             answer_ice = false;
@@ -234,22 +261,55 @@ public class StoreHall extends AppCompatActivity {
             answer_milk = false;
             answer_vanilla = false;
             answer_lemon = true;
+            answer_matcha = false;
+            answer_strawberry = false;
+        }
+        else if (order==10) {//아이스 녹차라떼
+            answer_ice = true;
+            answer_water = false;
+            answer_coffee = false;
+            answer_milk = true;
+            answer_vanilla = false;
+            answer_lemon = true;
+            answer_matcha = true;
+            answer_strawberry = false;
+        }
+        else if (order==11) {//핫 녹차라떼
+            answer_ice = false;
+            answer_water = false;
+            answer_coffee = false;
+            answer_milk = true;
+            answer_vanilla = false;
+            answer_lemon = true;
+            answer_matcha = true;
+            answer_strawberry = false;
+        }
+        else if (order==12) {// 딸기라떼
+            answer_ice = true;
+            answer_water = false;
+            answer_coffee = false;
+            answer_milk = true;
+            answer_vanilla = false;
+            answer_lemon = true;
+            answer_matcha = false;
+            answer_strawberry = true;
         }
 
-        Beverage beverage = new Beverage(answer_water, answer_milk, answer_coffee, answer_ice, answer_vanilla, answer_lemon);
+        Beverage beverage = new Beverage(answer_water, answer_milk, answer_coffee, answer_ice, answer_vanilla, answer_lemon, answer_matcha, answer_strawberry);
 
         return beverage;
     }
     private void showReview(Integer beverage_completion) {
 
-        if(beverage_completion==6) clientOrder.setText(R.string.completeMsg100);
-        else if(beverage_completion==5) clientOrder.setText(R.string.completeMsg85);
-        else if(beverage_completion==4) clientOrder.setText(R.string.completeMsg70);
-        else if(beverage_completion==3) clientOrder.setText(R.string.completeMsg55);
-        else if(beverage_completion==2) clientOrder.setText(R.string.completeMsg40);
-        else if(beverage_completion==1) clientOrder.setText(R.string.completeMsg25);
-        else if(beverage_completion==0) clientOrder.setText(R.string.completeMsg10);
-        else clientOrder.setText(R.string.completeMsg0);
+        if (beverage_completion==-1) clientOrder.setText(R.string.completeMsgNegative);
+        else if(beverage_completion==8) clientOrder.setText(R.string.completeMsg100);
+        else if(beverage_completion>=6) clientOrder.setText(R.string.completeMsg85);
+        //else if(beverage_completion==6) clientOrder.setText(R.string.completeMsg70);
+            //else if(beverage_completion>=4) clientOrder.setText(R.string.completeMsg55);
+        else if(beverage_completion>=4) clientOrder.setText(R.string.completeMsg40);
+        //else if(beverage_completion==3) clientOrder.setText(R.string.completeMsg25);
+        else if(beverage_completion>=2) clientOrder.setText(R.string.completeMsg10);
+        else if (beverage_completion>=0) clientOrder.setText(R.string.completeMsg0);
 
     }
     private void showOrder(Integer order) {
@@ -263,6 +323,9 @@ public class StoreHall extends AppCompatActivity {
         else if (order==7) clientOrder.setText(R.string.order7);
         else if (order==8) clientOrder.setText(R.string.order8);
         else if (order==9) clientOrder.setText(R.string.order9);
+        else if (order==10) clientOrder.setText(R.string.order10);
+        else if (order==11) clientOrder.setText(R.string.order11);
+        else if (order==12) clientOrder.setText(R.string.order12);
     }
     private void showClient(Integer order, Integer clientNum, Integer time, Integer getIntent, ImageView clientImg, TextView clientOrder, Button moveKitchen) {
         new Handler().postDelayed(new Runnable() {
@@ -292,11 +355,11 @@ public class StoreHall extends AppCompatActivity {
         }, time+1000);
 
     }
-    private void showWeather(Integer weather) {
+    private void showBackground(Integer weather) {
 
-        if(weather==0) backGroundImg.setBackgroundResource(R.drawable.hall_rain);
-        else if(weather==1) backGroundImg.setBackgroundResource(R.drawable.hall_day);
-        else if(weather==2) backGroundImg.setBackgroundResource(R.drawable.hall_night_rain);
+        if (weather==0) backGroundImg.setBackgroundResource(R.drawable.hall_rain);
+        else if (weather==1) backGroundImg.setBackgroundResource(R.drawable.hall_day);
+        else if (weather==2) backGroundImg.setBackgroundResource(R.drawable.hall_night_rain);
         else backGroundImg.setBackgroundResource(R.drawable.hall_night);
 
         Log.d("weather", "weather: " + weather);
--- app/src/main/java/com/example/coffeemaker/StoreKitchen.java
+++ app/src/main/java/com/example/coffeemaker/StoreKitchen.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +16,13 @@ public class StoreKitchen extends AppCompatActivity {
     //complete 버튼
     Button btnDone;
     //재료 토글버튼
-    ToggleButton water, milk, ice, coffee, vanilla, lemon;
+    ImageButton water, milk, ice, coffee, vanilla, lemon, matcha, strawberry;
     //firstIntent는 받아오는 인텐트, secondIntent는 보내는 인텐트
     Intent firstIntent, secondIntent;
     int order, client_num, weather_num;
 
     // boolean data for check order completion with result
-    boolean waterOn, milkOn, iceOn, coffeeOn, vanillaOn, lemonOn;
+    boolean waterOn, milkOn, iceOn, coffeeOn, vanillaOn, lemonOn, matchaOn, strawberryOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +37,131 @@ public class StoreKitchen extends AppCompatActivity {
         weather_num = firstIntent.getIntExtra("weather_index", -3);
 
         btnDone=(Button)findViewById(R.id.done_button);
-        water=(ToggleButton)findViewById(R.id.water_toggle);
-        milk=(ToggleButton)findViewById(R.id.milk_toggle);
-        ice=(ToggleButton)findViewById(R.id.ice_toggle);
-        coffee=(ToggleButton)findViewById(R.id.coffee_toggle);
-        vanilla=(ToggleButton)findViewById(R.id.vanilla_toggle);
-        lemon=(ToggleButton)findViewById(R.id.lemon_toggle);
+        water=(ImageButton)findViewById(R.id.water_toggle);
+        milk=(ImageButton)findViewById(R.id.milk_toggle);
+        ice=(ImageButton)findViewById(R.id.ice_toggle);
+        coffee=(ImageButton)findViewById(R.id.coffee_toggle);
+        vanilla=(ImageButton)findViewById(R.id.vanilla_toggle);
+        lemon=(ImageButton)findViewById(R.id.lemon_toggle);
+        matcha=(ImageButton)findViewById(R.id.matcha_toggle);
+        strawberry=(ImageButton)findViewById(R.id.strawberry_toggle);
 
+        water.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waterOn = !waterOn;
+                ViewGroup.LayoutParams params = water.getLayoutParams();
+                if (waterOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                water.setLayoutParams(params);
+            }
+        });
+        milk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {milkOn = !milkOn;
+                ViewGroup.LayoutParams params = milk.getLayoutParams();
+                if (milkOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                milk.setLayoutParams(params);}
+        });
+        ice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {iceOn = !iceOn;
+                ViewGroup.LayoutParams params = ice.getLayoutParams();
+                if (iceOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                ice.setLayoutParams(params);}
+        });
+        coffee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {coffeeOn = !coffeeOn;
+                ViewGroup.LayoutParams params = coffee.getLayoutParams();
+                if (coffeeOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                coffee.setLayoutParams(params);}
+        });
+        vanilla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {vanillaOn = !vanillaOn;
+                ViewGroup.LayoutParams params = vanilla.getLayoutParams();
+                if (vanillaOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                vanilla.setLayoutParams(params);}
+        });
+        lemon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {lemonOn = !lemonOn;
+                ViewGroup.LayoutParams params = lemon.getLayoutParams();
+                if (lemonOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                lemon.setLayoutParams(params);}
+        });
+        matcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {matchaOn = !matchaOn;
+                ViewGroup.LayoutParams params = matcha.getLayoutParams();
+                if (matchaOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                matcha.setLayoutParams(params);}
+        });
+        strawberry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {strawberryOn = !strawberryOn;
+                ViewGroup.LayoutParams params = strawberry.getLayoutParams();
+                if (strawberryOn) {
+                    params.width = 300;  // 원하는 너비 (픽셀 단위)
+                    params.height = 300; // 원하는 높이 (픽셀 단위)
+                } else {
+                    params.width = 200;  // 원래 너비 (픽셀 단위)
+                    params.height = 200; // 원래 높이 (픽셀 단위)
+                }
+                strawberry.setLayoutParams(params);}
+        });
         btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                waterOn=water.isChecked();
+                /*waterOn=water.isChecked();
                 milkOn= milk.isChecked();
                 iceOn=ice.isChecked();
                 coffeeOn=coffee.isChecked();
                 vanillaOn=vanilla.isChecked();
-                lemonOn=lemon.isChecked();
+                lemonOn=lemon.isChecked();*/
 
                 secondIntent = new Intent(context, StoreHall.class);
                 secondIntent.putExtra("is_waterOn", waterOn);
@@ -60,6 +170,8 @@ public class StoreKitchen extends AppCompatActivity {
                 secondIntent.putExtra("is_milkOn", milkOn);
                 secondIntent.putExtra("is_vanillaOn", vanillaOn);
                 secondIntent.putExtra("is_lemonOn", lemonOn);
+                secondIntent.putExtra("is_matchaOn", matchaOn);
+                secondIntent.putExtra("is_strawberryOn", strawberryOn);
                 secondIntent.putExtra("intent_index", 1);
                 secondIntent.putExtra("order_index", order);
                 secondIntent.putExtra("weather_index", weather_num);
